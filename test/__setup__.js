105eb4214436a4f06d39e65d123ec3f0ddcdc5d4
ProtoV1-OS <91277450+ProtoV1-OS@users.noreply.github.com>
2022-04-25 19:38:54 +0100

    Delete test directory

--- test/__setup__.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-jest.useFakeTimers();
-jest.spyOn(global, "setTimeout");
-jest.spyOn(global, "setInterval");
-
-const { db } = require("./__mock__").module;
-global.fetch = jest.fn(() => Promise.resolve(db));
